169061b40cb7b7d42e377ad5acb1b6c99dc62846
shenxia <shenxia@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2012-08-16 11:32:27 +0000

    Re-upload to remove warning messages.
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/bigrr@13 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/R/bigRR.R
+++ pkg/R/bigRR.R
@@ -2,3 +2,16 @@ bigRR <-
 function(formula = NULL, y = NULL, X = NULL, Z = NULL, data = NULL, shrink = NULL,
 lambda = NULL, tol.err = 1e-6, only.estimates = FALSE, ...) UseMethod("bigRR")
 
+.onAttach <- 
+		function(...)
+{
+	cat("bigRR: Fast generalized ridge regression for p >> n data\n")
+	cat('Version 1.3 installed\n')
+	cat('\n')
+	cat('Authors:    Xia Shen - xia.shen@icm.uu.se\n')
+	cat('            Moudud Alam - maa@du.se\n')
+	cat('            Lars Ronnegard - lrn@du.se\n')
+	cat('\n')
+	cat('Maintainer: Xia Shen - xia.shen@icm.uu.se\n')
+	options(warn = -1)
+}
--- pkg/R/hat.transf.R
+++ pkg/R/hat.transf.R
@@ -4,8 +4,8 @@ function(C22, transf, vc, w, k, N, tol.err = 1e-6) {
 		middle <- diag(N) - C22/vc
 		eigen.test <- eigen(middle, only.values = TRUE)
 		if (min(eigen.test$values) < tol.err) {
-			cat("Matrix middle bended\n")
-			cat("Minimum eigenvalue:\n", min(eigen.test$values))
+			## cat("Matrix middle bended\n")
+			## cat("Minimum eigenvalue:\n", min(eigen.test$values))
 			if (min(eigen.test$values) > (-tol.err)) middle <- middle + diag(N)*2*tol.err
 			if (min(eigen.test$values) < (-tol.err)) middle <- middle - (min(eigen.test$values) - tol.err)*diag(N)
 		}
