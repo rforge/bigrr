1d0bc197259f8057a03b4e57d0abe9f55f6b2ee1
shenxia <shenxia@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2013-02-15 17:29:21 +0000

    Update small display stuff including startup message command
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/bigrr@16 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/R/bigRR.default.R
+++ pkg/R/bigRR.default.R
@@ -1,6 +1,7 @@
 bigRR.default <-
 function (formula = NULL, y, X, Z, data = NULL, shrink = NULL, weight = NULL,
-    lambda = NULL, family = gaussian(link=identity), tol.err = 1e-6, only.estimates = FALSE, ...) 
+    lambda = NULL, family = gaussian(link=identity), impute = FALSE, tol.err = 1e-6, 
+	tol.conv = 1e-8, only.estimates = FALSE, GPU = FALSE, ...) 
 {
 
     Call <- match.call()
@@ -10,6 +11,26 @@ function (formula = NULL, y, X, Z, data = NULL, shrink = NULL, weight = NULL,
         stop("Z should be a matrix.")
     if (!(is.vector(y))) 
         stop("y should be a vector.")
+	if (GPU & !require(gputools)) stop('Package gputools is needed for using GPU.\n')
+	if (any(is.na(y))) {
+		naidx <- which(is.na(y))
+		y <- y[-naidx]
+		X <- X[-naidx,]
+		Z <- Z[-naidx,]
+	}
+	if (impute) {
+		if (any(is.na(Z))) {
+			cat('Imputing missing values...')
+			nacolidx <- which(is.na(colSums(Z)))
+			for (j in nacolidx) {
+				naidx <- which(is.na(Z[,j]))
+				Z[naidx,j] <- sample(Z[-naidx,j], length(naidx), TRUE)
+			}
+			cat('Done.\n')
+		} else {
+			cat("NOTE: no missing value exists, no need to impute.\n")
+		}
+	}
     N <- n <- nrow(X)
     p <- ncol(X)
     k <- ncol(Z)
@@ -17,7 +38,10 @@ function (formula = NULL, y, X, Z, data = NULL, shrink = NULL, weight = NULL,
         stop("Sizes of y, X, and Z are not all equal.")
     if (is.null(weight)) w <- rep(1, k) else w <- weight
 
-    G <- crossprod(sqrt(w)*t(Z)) ## bug fixed 111201 -- Xia
+    #G <- crossprod(sqrt(w)*t(Z)) ## bug fixed 111201 -- Xia
+	wZt <- sqrt(w)*t(Z)
+	if (!GPU) G <- crossprod(wZt) else G <- gpuMatMult(t(wZt), wZt)
+
 ############ Bending to allow for p<n problems -- Lars
     min.eigen <- min(eigen(G, only.values = TRUE)$values)
     if (min.eigen < tol.err) G <- G + diag(N)*(abs(min.eigen) + tol.err) 
@@ -40,14 +64,14 @@ function (formula = NULL, y, X, Z, data = NULL, shrink = NULL, weight = NULL,
     phi <- as.numeric(hm$varFix)
     sa <- as.numeric(hm$varRanef)
     a <- L%*%hm$ranef
-    tZinvG <- crossprod(Z, invG)
+	if (!GPU) tZinvG <- crossprod(Z, invG) else tZinvG <- gpuMatMult(t(Z), invG)
     u <- (w*tZinvG)%*%a
     qu <- GCV <- NULL
     if (!only.estimates) {
         C <- rbind(cbind(crossprod(X, X), crossprod(X, L)), cbind(crossprod(L, X), G + diag(N)*phi/sa))
         C22 <- solve(C)[(p + 1):(p + N), (p + 1):(p + N)]*phi
-        transf <- tcrossprod((w*tZinvG), t(L))
-        qu <- hat.transf(C22, transf, vc = sa, w, k, N, tol.err = tol.err)
+		if (!GPU) transf <- tcrossprod((w*tZinvG), t(L)) else transf <- gpuMatMult((w*tZinvG), L)
+        qu <- hat.transf(C22, transf, vc = sa, w, k, N, tol.err = tol.err, GPU = GPU)
         qu[qu < tol.err] <- tol.err
         qu[qu > (1 - tol.err)] <- 1 - tol.err
 	    if (family$family == "gaussian") GCV <- sum(hm$resid^2)/((n - sum(hm$hv[1:n]))^2)
